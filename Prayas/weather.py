95a79b4809fc236d2e68efb3dac223c6382fceb7
chittnoor <118926866+chittnoor@users.noreply.github.com>
2023-04-16 10:20:39 +0530

    Add files via upload

---
+++ Prayas/weather.py
@@ -0,0 +1,30 @@
+import requests
+import json
+
+# API key for OpenWeatherMap
+api_key = "3ae8e6cebf3576a23100de8d3449c274"
+
+# Get user's current location using IP address
+url = "http://ip-api.com/json/?fields=61439"
+response = requests.get(url)
+data = json.loads(response.text)
+lat = data['lat']
+lon = data['lon']
+
+# Make API call to OpenWeatherMap to get weather data
+url = f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric"
+response = requests.get(url)
+data = json.loads(response.text)
+print(data)
+print("\n \n")
+temp = data['main']['temp']
+humidity= data['main']['humidity']
+print(temp,"\n \n")
+print(humidity)
+duration=31
+if temp>=24 and temp<=30 and duration>=30:
+    if humidity>=16:
+        print("kello")
+    print(f"Your fan has been on for more than 30 minutes in ",temp,"  temperature \n")
+
+
